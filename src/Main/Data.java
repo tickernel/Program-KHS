fb525d3efa2e445c62bb2d76c17a4785e97533a7
Zhafron <48698292+tickernel@users.noreply.github.com>
2019-11-10 12:44:09 +0700

    add files via upload

---
+++ src/Main/Data.java
@@ -0,0 +1,726 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Main;
+
+/**
+ *
+ * @author EtaYuy
+ */
+public class Data
+{
+    private String dosenp;
+    private String nodosenp;
+    private String nama;
+    private String nim;
+    private String semester;
+    private String jurusan;
+    private String no1;
+    private String sandi1;
+    private String matkul1;
+    private float sks1;
+    private float nm1;
+    private String hm1;
+    private float kn1;
+    private String ket1;
+    private String no2;
+    private String sandi2;
+    private String matkul2;
+    private float sks2;
+    private float nm2;
+    private String hm2;
+    private float kn2;
+    private String ket2;
+    private String no3;
+    private String sandi3;
+    private String matkul3;
+    private float sks3;
+    private float nm3;
+    private String hm3;
+    private float kn3;
+    private String ket3;
+    private String no4;
+    private String sandi4;
+    private String matkul4;
+    private float sks4;
+    private float nm4;
+    private String hm4;
+    private float kn4;
+    private String ket4;
+    private String no5;
+    private String sandi5;
+    private String matkul5;
+    private float sks5;
+    private float nm5;
+    private String hm5;
+    private float kn5;
+    private String ket5;
+    private String no6;
+    private String sandi6;
+    private String matkul6;
+    private float sks6;
+    private float nm6;
+    private String hm6;
+    private float kn6;
+    private String ket6;
+    private String no7;
+    private String sandi7;
+    private String matkul7;
+    private float sks7;
+    private float nm7;
+    private String hm7;
+    private float kn7;
+    private String ket7;
+    private String no8;
+    private String sandi8;
+    private String matkul8;
+    private float sks8;
+    private float nm8;
+    private String hm8;
+    private float kn8;
+    private String ket8;
+    private float bebansks;
+    private float lulussks;
+    private float bebankum;
+    private float luluskum;
+    private float sksyad;
+    private float semangkamutu;
+    private float kumangkamutu;
+    private float ipsem;
+    private float ipkum;
+
+    public String getDosenp() {
+        return dosenp;
+    }
+
+    public void setDosenp(String dosenp) {
+        this.dosenp = dosenp;
+    }
+
+    public String getNodosenp() {
+        return nodosenp;
+    }
+
+    public void setNodosenp(String nodosenp) {
+        this.nodosenp = nodosenp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getNo1() {
+        return no1;
+    }
+
+    public void setNo1(String no1) {
+        this.no1 = no1;
+    }
+
+    public String getSandi1() {
+        return sandi1;
+    }
+
+    public void setSandi1(String sandi1) {
+        this.sandi1 = sandi1;
+    }
+
+    public String getMatkul1() {
+        return matkul1;
+    }
+
+    public void setMatkul1(String matkul1) {
+        this.matkul1 = matkul1;
+    }
+
+    public float getSks1() {
+        return sks1;
+    }
+
+    public void setSks1(float sks1) {
+        this.sks1 = sks1;
+    }
+
+    public float getNm1() {
+        return nm1;
+    }
+
+    public void setNm1(float nm1) {
+        this.nm1 = nm1;
+    }
+
+    public String getHm1() {
+        return hm1;
+    }
+
+    public void setHm1(String hm1) {
+        this.hm1 = hm1;
+    }
+
+    public float getKn1() {
+        return kn1;
+    }
+
+    public void setKn1(float kn1) {
+        this.kn1 = kn1;
+    }
+
+    public String getKet1() {
+        return ket1;
+    }
+
+    public void setKet1(String ket1) {
+        this.ket1 = ket1;
+    }
+
+    public String getNo2() {
+        return no2;
+    }
+
+    public void setNo2(String no2) {
+        this.no2 = no2;
+    }
+
+    public String getSandi2() {
+        return sandi2;
+    }
+
+    public void setSandi2(String sandi2) {
+        this.sandi2 = sandi2;
+    }
+
+    public String getMatkul2() {
+        return matkul2;
+    }
+
+    public void setMatkul2(String matkul2) {
+        this.matkul2 = matkul2;
+    }
+
+    public float getSks2() {
+        return sks2;
+    }
+
+    public void setSks2(float sks2) {
+        this.sks2 = sks2;
+    }
+
+    public float getNm2() {
+        return nm2;
+    }
+
+    public void setNm2(float nm2) {
+        this.nm2 = nm2;
+    }
+
+    public String getHm2() {
+        return hm2;
+    }
+
+    public void setHm2(String hm2) {
+        this.hm2 = hm2;
+    }
+
+    public float getKn2() {
+        return kn2;
+    }
+
+    public void setKn2(float kn2) {
+        this.kn2 = kn2;
+    }
+
+    public String getKet2() {
+        return ket2;
+    }
+
+    public void setKet2(String ket2) {
+        this.ket2 = ket2;
+    }
+
+    public String getNo3() {
+        return no3;
+    }
+
+    public void setNo3(String no3) {
+        this.no3 = no3;
+    }
+
+    public String getSandi3() {
+        return sandi3;
+    }
+
+    public void setSandi3(String sandi3) {
+        this.sandi3 = sandi3;
+    }
+
+    public String getMatkul3() {
+        return matkul3;
+    }
+
+    public void setMatkul3(String matkul3) {
+        this.matkul3 = matkul3;
+    }
+
+    public float getSks3() {
+        return sks3;
+    }
+
+    public void setSks3(float sks3) {
+        this.sks3 = sks3;
+    }
+
+    public float getNm3() {
+        return nm3;
+    }
+
+    public void setNm3(float nm3) {
+        this.nm3 = nm3;
+    }
+
+    public String getHm3() {
+        return hm3;
+    }
+
+    public void setHm3(String hm3) {
+        this.hm3 = hm3;
+    }
+
+    public float getKn3() {
+        return kn3;
+    }
+
+    public void setKn3(float kn3) {
+        this.kn3 = kn3;
+    }
+
+    public String getKet3() {
+        return ket3;
+    }
+
+    public void setKet3(String ket3) {
+        this.ket3 = ket3;
+    }
+
+    public String getNo4() {
+        return no4;
+    }
+
+    public void setNo4(String no4) {
+        this.no4 = no4;
+    }
+
+    public String getSandi4() {
+        return sandi4;
+    }
+
+    public void setSandi4(String sandi4) {
+        this.sandi4 = sandi4;
+    }
+
+    public String getMatkul4() {
+        return matkul4;
+    }
+
+    public void setMatkul4(String matkul4) {
+        this.matkul4 = matkul4;
+    }
+
+    public float getSks4() {
+        return sks4;
+    }
+
+    public void setSks4(float sks4) {
+        this.sks4 = sks4;
+    }
+
+    public float getNm4() {
+        return nm4;
+    }
+
+    public void setNm4(float nm4) {
+        this.nm4 = nm4;
+    }
+
+    public String getHm4() {
+        return hm4;
+    }
+
+    public void setHm4(String hm4) {
+        this.hm4 = hm4;
+    }
+
+    public float getKn4() {
+        return kn4;
+    }
+
+    public void setKn4(float kn4) {
+        this.kn4 = kn4;
+    }
+
+    public String getKet4() {
+        return ket4;
+    }
+
+    public void setKet4(String ket4) {
+        this.ket4 = ket4;
+    }
+
+    public String getNo5() {
+        return no5;
+    }
+
+    public void setNo5(String no5) {
+        this.no5 = no5;
+    }
+
+    public String getSandi5() {
+        return sandi5;
+    }
+
+    public void setSandi5(String sandi5) {
+        this.sandi5 = sandi5;
+    }
+
+    public String getMatkul5() {
+        return matkul5;
+    }
+
+    public void setMatkul5(String matkul5) {
+        this.matkul5 = matkul5;
+    }
+
+    public float getSks5() {
+        return sks5;
+    }
+
+    public void setSks5(float sks5) {
+        this.sks5 = sks5;
+    }
+
+    public float getNm5() {
+        return nm5;
+    }
+
+    public void setNm5(float nm5) {
+        this.nm5 = nm5;
+    }
+
+    public String getHm5() {
+        return hm5;
+    }
+
+    public void setHm5(String hm5) {
+        this.hm5 = hm5;
+    }
+
+    public float getKn5() {
+        return kn5;
+    }
+
+    public void setKn5(float kn5) {
+        this.kn5 = kn5;
+    }
+
+    public String getKet5() {
+        return ket5;
+    }
+
+    public void setKet5(String ket5) {
+        this.ket5 = ket5;
+    }
+
+    public String getNo6() {
+        return no6;
+    }
+
+    public void setNo6(String no6) {
+        this.no6 = no6;
+    }
+
+    public String getSandi6() {
+        return sandi6;
+    }
+
+    public void setSandi6(String sandi6) {
+        this.sandi6 = sandi6;
+    }
+
+    public String getMatkul6() {
+        return matkul6;
+    }
+
+    public void setMatkul6(String matkul6) {
+        this.matkul6 = matkul6;
+    }
+
+    public float getSks6() {
+        return sks6;
+    }
+
+    public void setSks6(float sks6) {
+        this.sks6 = sks6;
+    }
+
+    public float getNm6() {
+        return nm6;
+    }
+
+    public void setNm6(float nm6) {
+        this.nm6 = nm6;
+    }
+
+    public String getHm6() {
+        return hm6;
+    }
+
+    public void setHm6(String hm6) {
+        this.hm6 = hm6;
+    }
+
+    public float getKn6() {
+        return kn6;
+    }
+
+    public void setKn6(float kn6) {
+        this.kn6 = kn6;
+    }
+
+    public String getKet6() {
+        return ket6;
+    }
+
+    public void setKet6(String ket6) {
+        this.ket6 = ket6;
+    }
+
+    public String getNo7() {
+        return no7;
+    }
+
+    public void setNo7(String no7) {
+        this.no7 = no7;
+    }
+
+    public String getSandi7() {
+        return sandi7;
+    }
+
+    public void setSandi7(String sandi7) {
+        this.sandi7 = sandi7;
+    }
+
+    public String getMatkul7() {
+        return matkul7;
+    }
+
+    public void setMatkul7(String matkul7) {
+        this.matkul7 = matkul7;
+    }
+
+    public float getSks7() {
+        return sks7;
+    }
+
+    public void setSks7(float sks7) {
+        this.sks7 = sks7;
+    }
+
+    public float getNm7() {
+        return nm7;
+    }
+
+    public void setNm7(float nm7) {
+        this.nm7 = nm7;
+    }
+
+    public String getHm7() {
+        return hm7;
+    }
+
+    public void setHm7(String hm7) {
+        this.hm7 = hm7;
+    }
+
+    public float getKn7() {
+        return kn7;
+    }
+
+    public void setKn7(float kn7) {
+        this.kn7 = kn7;
+    }
+
+    public String getKet7() {
+        return ket7;
+    }
+
+    public void setKet7(String ket7) {
+        this.ket7 = ket7;
+    }
+
+    public String getNo8() {
+        return no8;
+    }
+
+    public void setNo8(String no8) {
+        this.no8 = no8;
+    }
+
+    public String getSandi8() {
+        return sandi8;
+    }
+
+    public void setSandi8(String sandi8) {
+        this.sandi8 = sandi8;
+    }
+
+    public String getMatkul8() {
+        return matkul8;
+    }
+
+    public void setMatkul8(String matkul8) {
+        this.matkul8 = matkul8;
+    }
+
+    public float getSks8() {
+        return sks8;
+    }
+
+    public void setSks8(float sks8) {
+        this.sks8 = sks8;
+    }
+
+    public float getNm8() {
+        return nm8;
+    }
+
+    public void setNm8(float nm8) {
+        this.nm8 = nm8;
+    }
+
+    public String getHm8() {
+        return hm8;
+    }
+
+    public void setHm8(String hm8) {
+        this.hm8 = hm8;
+    }
+
+    public float getKn8() {
+        return kn8;
+    }
+
+    public void setKn8(float kn8) {
+        this.kn8 = kn8;
+    }
+
+    public String getKet8() {
+        return ket8;
+    }
+
+    public void setKet8(String ket8) {
+        this.ket8 = ket8;
+    }
+
+    public float getBebansks() {
+        return bebansks;
+    }
+
+    public void setBebansks(float bebansks) {
+        this.bebansks = bebansks;
+    }
+
+    public float getLulussks() {
+        return lulussks;
+    }
+
+    public void setLulussks(float lulussks) {
+        this.lulussks = lulussks;
+    }
+
+    public float getBebankum() {
+        return bebankum;
+    }
+
+    public void setBebankum(float bebankum) {
+        this.bebankum = bebankum;
+    }
+
+    public float getLuluskum() {
+        return luluskum;
+    }
+
+    public void setLuluskum(float luluskum) {
+        this.luluskum = luluskum;
+    }
+
+    public float getSksyad() {
+        return sksyad;
+    }
+
+    public void setSksyad(float sksyad) {
+        this.sksyad = sksyad;
+    }
+
+    public float getSemangkamutu() {
+        return semangkamutu;
+    }
+
+    public void setSemangkamutu(float semangkamutu) {
+        this.semangkamutu = semangkamutu;
+    }
+
+    public float getKumangkamutu() {
+        return kumangkamutu;
+    }
+
+    public void setKumangkamutu(float kumangkamutu) {
+        this.kumangkamutu = kumangkamutu;
+    }
+
+    public float getIpsem() {
+        return ipsem;
+    }
+
+    public void setIpsem(float ipsem) {
+        this.ipsem = ipsem;
+    }
+
+    public float getIpkum() {
+        return ipkum;
+    }
+
+    public void setIpkum(float ipkum) {
+        this.ipkum = ipkum;
+    }
+
+}
